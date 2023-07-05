338275706b7a61885d2629b61a4c6cff8030b548
brwnalima <bruna.lima@edu.unirio.br>
2023-07-05 20:01:02 -0300

    responsividade do navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import './Navbar.css';
-import { Slide } from "react-awesome-reveal";
+import { MdClose } from 'react-icons/md';
 
 
 function Navbar() {
@@ -11,14 +11,9 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  let screenWidth = window.innerWidth;
-  let direction;
-
-  if (screenWidth < 768) {
-    direction = "right";
-  } else {
-    direction = "up";
-  }
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <nav className="navbar">
@@ -26,17 +21,19 @@ function Navbar() {
         <img src="https://www.pngmart.com/files/22/The-Last-Of-Us-Logo-PNG.png" />
 
       </div>
-      <Slide direction={direction}>
-        <div className={`navbar-center ${isMenuOpen ? 'navbar-menu-open' : ''}`}>
-          <a href="#">HOME</a>
-          <a href="#">ABOUT</a>
-          <a href="#game">GAME</a>
-          <a href="#serie">SERIES</a>
-        </div>
-      </Slide>
+
+      <div className={`navbar-center ${isMenuOpen ? 'navbar-menu-open' : ''}`}>
+        <a href="#" onClick={closeMenu}>HOME</a>
+        <a href="#" onClick={closeMenu}>ABOUT</a>
+        <a href="#game" onClick={closeMenu}>GAME</a>
+        <a href="#serie" onClick={closeMenu}>SERIES</a>
+      </div>
+
       <div className="navbar-right"><img src="https://www.gran-turismo.com/gtsport/decal/8079479801260311040_1.png" /></div>
       <div className="navbar-toggle" onClick={toggleMenu}>
-        <FiMenu />
+
+        { isMenuOpen ? <MdClose className='svg'/> : <FiMenu className='svg'/>}
+        
       </div>
     </nav>
   );
